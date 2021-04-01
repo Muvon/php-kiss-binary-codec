d2fc30ab038d8abdc60f08a4152429e924efb348
Don Hardman <don.hardman@muvon.io>
2021-04-02 06:14:21 +0700

    Change pattern and full logic

--- README.md
+++ README.md
@@ -2,65 +2,41 @@
 
 Binary codec for encoding multiple structures with predefined config
 
+It works only with array as input cuz created for complex structure and detect best encoding option for data and return mostly optimized data for hashes and ints with big arrays
+
 ## Encoder flow
 
-Each encoded field has next structure
+Encoder uses pack to encode and unpack to decode with special modifications and extra addons.
 
-- 1 byte - binary type
-- 1 byte - binary key
-- 1-4 byte - size of binary encoded data depends on binary type
-- x byte - body
+Binary contains of data
 
-## Supported binary types
+- 4 bytes size of meta
+- x bytes meta data with pack info and keys encoded with gzip
+- x bytes encoded binary data
 
-Binary types defined as contants with prefix BC_*
+## Sample of usage
 
-Each binary type has own max size definition that prepended to binary result of packed data.
+```php
+use Muvon\KISS\BinaryCodec;
+$data = json_decode('[null,[{"hash":"80ca095ed10b02e53d769eb6eaf92cd04e9e0759e5be4a8477b42911ba49c78f","strippedsize":215,"size":215,"weight":860,"height":1,"version":1,"versionHex":"00000001","merkleroot":"fa3906a4219078364372d0e2715f93e822edd0b47ce146c71ba7ba57179b50f6","tx":["fa3906a4219078364372d0e2715f93e822edd0b47ce146c71ba7ba57179b50f6"],"time":1318055359,"mediantime":1318055359,"nonce":272255,"bits":"1e0ffff0","difficulty":"0.000244140625","chainwork":"0000000000000000000000000000000000000000000000000000000000200020","nTx":1,"previousblockhash":"12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2","nextblockhash":"13957807cdd1d02f993909fa59510e318763f99a506c4c426e3b254af09f40d7"},{"hash":"a4ad8c78654e76a729c12dd10522a40ba7077167bbf5b28fc9c71d25fcd67226","strippedsize":215,"size":215,"weight":860,"height":20,"version":1,"versionHex":"00000001","merkleroot":"732300fac0d661a2bf01ef2f521460cc26eeb608939c196ab34221f6533d60f7","tx":["732300fac0d661a2bf01ef2f521460cc26eeb608939c196ab34221f6533d60f7"],"time":1318474885,"mediantime":1318474854,"nonce":1164,"bits":"1e0ffff0","difficulty":"0.000244140625","chainwork":"0000000000000000000000000000000000000000000000000000000001500150","nTx":1,"previousblockhash":"6ea63d7aa791c13042b72c6855f089ef78ed45c3b8dc3263fc0e8cb8b3070f05","nextblockhash":"31ed087d6effc647d37b6e04f7e7da9e04b270d95262b21c8275aa10fc0c7104"},{"hash":"537ad94bc3b5927e32cdb6bc2d59adf54a010901e45f1602738a709d4d67f53b","strippedsize":215,"size":215,"weight":860,"height":3422,"version":1,"versionHex":"00000001","merkleroot":"003632bba01886ab81df69d734665ec00562f6363e82286c4ace45d700d86f8e","tx":["003632bba01886ab81df69d734665ec00562f6363e82286c4ace45d700d86f8e"],"time":1318483933,"mediantime":1318483922,"nonce":1246,"bits":"1e0fffff","difficulty":"0.0002441371325370145","chainwork":"00000000000000000000000000000000000000000000000000000000d5f0837f","nTx":1,"previousblockhash":"4e2695a5d2d0c05ba6c264a51cea731cd61567b66e54cb0450f05b169fc27ea5","nextblockhash":"2707f4d6ca57db167afb88b98e97f8956d4c34cc9c6cc5155ad1e48330278241"}]]', true);
 
-Here is full list of contants
+var_dump(BinaryCodec::create()->pack($data));
 
-| Constant | Max size | Info |
-|-|-|-|
-| BC_RAW | 4 bytes | Raw data representation |
-| BC_BOOL | 1 byte | Boolean can be true or false only |
-| BC_INT1 | 1 byte | 8 bit signed integer |
-| BC_UINT1 | 1 byte | 8 bit unsigned integer |
-| BC_INT2 | 1 byte | 16 bit signed integer |
-| BC_UINT2 | 1 byte | 16 bit unsigned integer |
-| BC_INT4 | 1 byte | 32 bit signed integer |
-| BC_UINT4 | 1 byte | 32 bit unsigned integer |
-| BC_INT8 | 1 byte | 64 bit signed integer |
-| BC_UINT8 | 1 byte | 64 bit unsigned integer |
-| BC_FLOAT | 2 bytes | Float |
-| BC_DOUBLE | 2 bytes | Actually same as float in PHP |
-| BC_HEX | 4 bytes | Hex encoded value |
-| BC_CHAR | 1 byte | 8 bit signed char |
-| BC_UCHAR | 1 byte | 8 bit unsigned char |
-| BC_STR | 4 bytes | Just string data |
-| BC_LIST | 4 bytes | Simple array that represent list with indexed keys |
-| BC_LIST_UINT1 | 4 bytes | List of unsigned 8 bit integers |
-| BC_LIST_UINT2 | 4 bytes | List of unsigned 16 bit integers |
-| BC_LIST_UINT4 | 4 bytes | List of unsigned 32 bit integers |
-| BC_LIST_UINT8 | 4 bytes | List of unsigned 64 bit integers |
-| BC_LIST_INT1 | 4 bytes | List of signed 8 bit integers |
-| BC_LIST_INT2 | 4 bytes | List of signed 16 bit integers |
-| BC_LIST_INT4 | 4 bytes | List of signed 32 bit integers |
-| BC_LIST_INT8 | 4 bytes | List of signed 64 bit integers |
-| BC_LIST_HEX16 | 4 bytes | List of hex strings 16 bytes length (32 bytes as string) |
-| BC_LIST_HEX32 | 4 bytes | List of hex strings 32 bytes length (64 bytes as string) |
-| BC_LIST_HEX64 | 4 bytes | List of hex strings 64 bytes length (128 bytes as string) |
-| BC_LIST_STR | 4 bytes | List of string that concatenates with null byte and explode by it |
-| BC_MAP | 4 bytes | Hash-map that is simple array but with associative keys |
-| BC_NUM | 1 byte | Actually string representation of real big integer |
-| BC_NULL | 1 byte | NULL value |
+// string(983)
 
-## Binary keys
+var_dump(msgpack_pack($data));
 
-Binary key are defined by configuration passed in codec and cannot exceed amount of 256 types total.
+// string(1844)
+```
 
-## Size of binary
+## Unique features
 
-Size is presented in bytes for encoded data.
+The coded uses automatic detection data and encode is with best way.
+
+1. Hex strings (transaction hash for example)
+2. Big numeric as string converted to binary
+3. Hash map extracts all keys and converted as plain
+4. Int size determined by input value and it uses smaller one
 
 ## Test coverage
 
--- src/BinaryCodec.php
+++ src/BinaryCodec.php
@@ -3,357 +3,267 @@ namespace Muvon\KISS;
 
 use Error;
 
-// Max \x1f
-define('BC_RAW',    "\x00");
+// Max \x06
 define('BC_BOOL',   "\x01");
-define('BC_INT1',   "\x12");
-define('BC_INT2',   "\x02");
-define('BC_INT4',   "\x03");
-define('BC_INT8',   "\x04");
-define('BC_UINT1',  "\x13");
-define('BC_UINT2',  "\x05");
-define('BC_UINT4',  "\x06");
-define('BC_UINT8',  "\x07");
-define('BC_FLOAT',  "\x08");
-define('BC_DOUBLE', "\x09");
-define('BC_HEX',    "\x0a");
-define('BC_CHAR',   "\x0b");
-define('BC_UCHAR',  "\x0c");
-define('BC_STR',    "\x0d");
-define('BC_LIST',   "\x0e");
-define('BC_LIST_UINT1', "\x14");
-define('BC_LIST_UINT2', "\x15");
-define('BC_LIST_UINT4', "\x16");
-define('BC_LIST_UINT8', "\x1f");
-define('BC_LIST_INT1', "\x17");
-define('BC_LIST_INT2', "\x18");
-define('BC_LIST_INT4', "\x19");
-define('BC_LIST_INT8', "\x1a");
-define('BC_LIST_HEX16', "\x1b");
-define('BC_LIST_HEX32', "\x1c");
-define('BC_LIST_HEX64', "\x1d");
-define('BC_LIST_STR', "\x1e");
-define('BC_MAP',    "\x0f");
-define('BC_NUM',    "\x10");
-define('BC_NULL',   "\x11");
-
+define('BC_LIST',   "\x02");
+define('BC_KEY',    "\x03");
+define('BC_NUM',    "\x04");
+define('BC_NULL',   "\x05");
+define('BC_HASH',   "\x06");
 final class BinaryCodec {
-  const PK_MAP = [
-    1 => 'C',
-    2 => 'n',
-    4 => 'N',
-  ];
-
-  const SZ_MAP = [
-    BC_RAW => 4,
-    BC_BOOL => 1,
-    BC_INT1 => 1,
-    BC_INT2 => 1,
-    BC_INT4 => 1,
-    BC_INT8 => 1,
-    BC_UINT1 => 1,
-    BC_UINT2 => 1,
-    BC_UINT4 => 1,
-    BC_UINT8 => 1,
-    BC_FLOAT => 2,
-    BC_DOUBLE => 2,
-    BC_HEX => 4,
-    BC_CHAR => 1,
-    BC_UCHAR => 1,
-    BC_STR => 4,
-    BC_LIST => 4,
-    BC_LIST_UINT1 => 4,
-    BC_LIST_UINT2 => 4,
-    BC_LIST_UINT4 => 4,
-    BC_LIST_UINT8 => 4,
-    BC_LIST_INT1 => 4,
-    BC_LIST_INT2 => 4,
-    BC_LIST_INT4 => 4,
-    BC_LIST_INT8 => 4,
-    BC_LIST_HEX16 => 4,
-    BC_LIST_HEX32 => 4,
-    BC_LIST_HEX64 => 4,
-    BC_LIST_STR => 4,
-    BC_MAP => 4,
-    BC_NUM => 1,
-    BC_NULL => 1,
-  ];
-
   protected array $key_map;
+  protected array $format;
 
-  protected final function __construct(protected array $config) {
-    $this->key_map = array_flip(array_keys($config));
+  protected final function __construct() {}
+
+  public static function create(): self {
+    return new static;
   }
 
-  public static function create(array $config): self {
-    return new static($config);
+  public function pack(array $data): string {
+    $this->format = [];
+    $this->key_map = [];
+
+    $parts = [];
+    $this->format[] = (key($data) === 0 ? BC_LIST : BC_HASH). 'N';
+    $parts = [sizeof($data)];
+
+    array_push($parts, ...$this->encode($data));
+    $format_str = implode('/', $this->format);
+    $flow = gzencode($format_str . "\0" . implode("/", $this->key_map));
+    return pack(
+      'Na*' . strtr($format_str, [
+        '/' => '', BC_HASH => '', BC_KEY => '',
+        BC_LIST => '', BC_BOOL => '', BC_NULL => '',
+        BC_NUM => ''
+      ]),
+      strlen($flow),
+      $flow,
+      ...$parts
+    );
   }
 
-  public function pack(mixed $data): string {
-    return $this->encode($data, $this->getDataType($data));
+  protected function encode(mixed $data): array {
+    if (is_array($data)) {
+      $parts = [];
+      foreach ($data as $k => $v) {
+        // First we do hash map key cuz it can contain list
+        if (is_string($k)) {
+          if (!in_array($k, $this->key_map)) {
+            $this->key_map[] = $k;
+            $idx = sizeof($this->key_map) - 1;
+          } else {
+            $idx = array_search($k, $this->key_map);
+          }
+          $this->format[] = BC_KEY . 'C';
+          array_push($parts, $idx);
+        }
+
+        // Can be list or hash array
+        if (is_array($v)) {
+          $sz = sizeof($v);
+          if (key($v) === 0 || $sz === 0) {
+            $this->format[] = BC_LIST . 'N';
+            array_push($parts, $sz);
+          } else {
+            $this->format[] = BC_HASH . 'N';
+            array_push($parts, $sz);
+          }
+        }
+
+        array_push($parts, ...$this->encode($v));
+      }
+
+      return $parts;
+    } else {
+      $this->format[] = $this->getDataFormat($data);
+      return [$data];
+    }
   }
 
   public function unpack(string $binary): mixed {
-    $k_type = $binary[0];
-    $k_key = $this->decodeKeyName($binary[1]) ?: null;
-    $sz_len = static::SZ_MAP[$k_type];
-    $k_len = unpack(static::PK_MAP[$sz_len], substr($binary, 2, $sz_len))[1];
-    $meta_len = 2 + $sz_len;
-    if ($k_len !== (strlen($binary) - $meta_len)) {
-      throw new Error('Inconsistent binary data passed');
-    }
-
-    return $k_len
-      ? $this->decode(substr($binary, $meta_len), $k_type, $k_key)
-      : null
-    ;
+    $meta_len = hexdec(bin2hex($binary[0] . $binary[1] . $binary[2] . $binary[3]));
+    return $this->decode(gzdecode(substr($binary, 4, $meta_len)), substr($binary, 4 + $meta_len));
   }
 
-  protected function encode(mixed $data, string $type, ?string $key = "\x00"): string {
-    $sz_len = static::SZ_MAP[$type];
-    $sz_fmt = static::PK_MAP[$sz_len];
-    switch ($type) {
-      case BC_LIST:
-      case BC_MAP:
-        $bin = b'';
-        foreach ($data as $k => &$v) {
-          $k_type = $type === BC_MAP ? $this->getKeyType($k) : BC_RAW;
-          if ($k_type === BC_RAW) {
-            $k_type = $this->getDataType($v);
-          }
-          $bin .= $this->encode($v, $k_type, $this->encodeKeyName($k));
-        }
-
-        return $type . $key . pack($sz_fmt, strlen($bin)) . $bin;
-        break;
-
-      case BC_LIST_INT1:
-      case BC_LIST_INT2:
-      case BC_LIST_INT4:
-      case BC_LIST_INT8:
-      case BC_LIST_UINT1:
-      case BC_LIST_UINT2:
-      case BC_LIST_UINT4:
-      case BC_LIST_UINT8:
-        $bin = pack($this->getPackType($type), ...$data);
-        return $type . $key . pack($sz_fmt, strlen($bin)) . $bin;
-        break;
-
-      case BC_LIST_HEX16:
-      case BC_LIST_HEX32:
-      case BC_LIST_HEX64:
-        $bin = pack(str_repeat('H*', sizeof($data)), ...$data);
-        return $type . $key . pack($sz_fmt, strlen($bin)) . $bin;
-        break;
-
-      case BC_LIST_STR:
-        $bin = implode("\0", $data);
-        return $type . $key . pack($sz_fmt, strlen($bin)) . $bin;
-        break;
-
-      case BC_BOOL:
-        return $type . $key . pack($sz_fmt, 1) . ($data ? "\x01" : "\x00");
-        break;
-
-      case BC_HEX:
-      case BC_CHAR:
-      case BC_UCHAR:
-      case BC_INT2:
-      case BC_INT4:
-      case BC_INT8:
-      case BC_UINT2:
-      case BC_UINT4:
-      case BC_UINT8:
-      case BC_FLOAT:
-      case BC_DOUBLE:
-      case BC_INT1:
-      case BC_UINT1:
-        return $this->packType($data, $type, $key);
-        break;
-
-      case BC_NUM:
-        $val = gmp_strval(gmp_init($data, 10), 16);
-        if (strlen($val) % 2 !== 0) {
-          $val = '0' . $val;
-        }
-        $bin = hex2bin($val);
-        return $type . $key . pack($sz_fmt, strlen($bin)) . $bin;
-        break;
-
-      // case BC_STR:
-      //   $bin = gzencode($data);
-      //   return $type . $key .  pack(static::SIZE_TYPE, strlen($bin)) . $bin;
-      //   break;
-
-      case BC_NULL:
-        return $type. pack($sz_fmt, 1) . "\x00";
-        break;
-
-      default:
-        return $type . $key . pack($sz_fmt, strlen($data)) . $data;
-    }
-  }
-
-  protected function decode(string $binary, string $type, ?string $key = null): mixed {
-    $sz_len = static::SZ_MAP[$type];
-    $sz_fmt = static::PK_MAP[$sz_len];
-    switch ($type) {
-      case BC_LIST:
-      case BC_MAP:
-        $max_index = strlen($binary);
-        $i = 0;
-        $data = $key ? [$key => []] : [];
-        if ($key) {
-          $list = &$data[$key];
-        } else {
-          $list = &$data;
-        }
-
-        while ($i < $max_index) {
-          $k_type = $binary[$i];
-          $sz_len = static::SZ_MAP[$k_type];
-          $sz_fmt = static::PK_MAP[$sz_len];
-
-          $k_key = $this->decodeKeyName($binary[$i + 1]) ?: null;
-          $meta_len = 2 + $sz_len;
-          $k_len = unpack($sz_fmt, substr($binary, $i + 2, $sz_len))[1];
-          if ($k_key) {
-            $list[$k_key] = $this->decode(substr($binary, $i + $meta_len, $k_len), $k_type, $k_key);
-          } else {
-            $list[] = $this->decode(substr($binary, $i + $meta_len, $k_len), $k_type, $k_key);
-          }
-          $i += $k_len + $meta_len;
-        }
-        return $list;
-
-        break;
-
-      case BC_BOOL:
-        return $binary === "\x01";
-        break;
-
-      case BC_HEX:
-      case BC_CHAR:
-      case BC_UCHAR:
-      case BC_INT1:
-      case BC_INT2:
-      case BC_INT4:
-      case BC_INT8:
-      case BC_UINT1:
-      case BC_UINT2:
-      case BC_UINT4:
-      case BC_UINT8:
-      case BC_FLOAT:
-      case BC_DOUBLE:
-        return $this->unpackType($binary, $type);
-        break;
-
-      case BC_LIST_INT1:
-      case BC_LIST_INT2:
-      case BC_LIST_INT4:
-      case BC_LIST_INT8:
-      case BC_LIST_UINT1:
-      case BC_LIST_UINT2:
-      case BC_LIST_UINT4:
-      case BC_LIST_UINT8:
-        return array_values(unpack($this->getPackType($type), $binary));
-        break;
-
-      case BC_LIST_HEX16:
-      case BC_LIST_HEX32:
-      case BC_LIST_HEX64:
-        $sz = match($type) {
-          BC_LIST_HEX16 => 32,
-          BC_LIST_HEX32 => 64,
-          BC_LIST_HEX64 => 128,
+  protected function decode(string $meta, string $binary): mixed {
+    $i = 0;
+    $keys = [];
+    $format = strtok($meta, "\0");
+    $key_map = explode('/', strtok("\0"));
+    $format = implode('/', array_map(
+      function ($item) use (&$i, &$keys) {
+        $key = 'n' . ($i++);
+        $prefix = match ($item[0]) {
+          BC_NULL, BC_NUM, BC_BOOL, BC_LIST, BC_KEY, BC_HASH => substr($item, 1),
+          default => $item,
         };
-        return str_split(bin2hex($binary), $sz);
-        break;
 
-      case BC_LIST_STR:
-        return explode("\0", $binary);
+        if ($prefix !== $item) {
+          $keys[$key] = $item[0];
+        }
 
-      case BC_NUM:
-        return gmp_strval(gmp_init(bin2hex($binary), 16), 10);
-        break;
+        return $prefix . $key;
+      },
+      explode('/', $format)
+    ));
+    $result = null;
+    $ref = &$result;
+    $path = [];
+    $ns = [];
+    $i = 0;
+    $n = 0;
 
-      // case BC_STR:
-      //   return gzdecode($binary);
-      //   break;
+    $prev = null;
+    foreach (unpack($format, $binary) as $k => &$v) {
+      if (isset($keys[$k])) {
+        $v = match ($keys[$k]) {
+          BC_NULL => null,
+          BC_NUM => gmp_strval(gmp_init(bin2hex($v), 16)),
+          BC_BOOL => !!$v,
+          default => $v,
+        };
 
-      case BC_NULL:
-        return null;
-        break;
+        if ($keys[$k] === BC_LIST) {
+          $ns[] = $v;
+          if (!isset($ref)) {
+            $ref = [];
+            $path[] = '-';
+          } else {
+            $ref[$i] = [];
+            $ref = &$ref[$i];
+            $path[] = $i;
+          }
 
-      default:
-        return $binary;
+          $i = 0;
+          continue;
+        }
+
+        if ($keys[$k] === BC_HASH) {
+          $ns[] = $v;
+          if (is_array($ref)) {
+            $ref[$i] = [];
+            $ref = &$ref[$i];
+            $path[] = $i;
+            // $i++;
+          } else {
+            $ref = [];
+            $path[] = '-';
+          }
+          continue;
+        }
+
+        if ($keys[$k] === BC_KEY) {
+          $h_key = $key_map[$v];
+          $ref = &$ref[$h_key];
+          $ns[]   = 1;
+          $path[] = $h_key;
+          continue;
+        }
+
+        $prev = &$keys[$k];
+      }
+
+      // Write data to ref logic
+      if (is_array($ref)) {
+        $ref[$i++] = $v;
+      } else {
+        $ref = $v;
+      }
+
+      // Reset ref pointer logic
+      $n = &$ns[array_key_last($ns)];
+      if (--$n === 0) {
+        do {
+          array_pop($path);
+          array_pop($ns);
+          $n = &$ns[array_key_last($ns)];
+        } while(--$n === 0);
+
+        $ref = &$result;
+        foreach ($path as $p) {
+          if ($p === '-') {
+            continue;
+          }
+          $ref = &$ref[$p];
+        }
+
+        if ($ref) {
+          $i = sizeof($ref);
+        }
+      }
     }
+    return $result;
   }
 
-  protected function packType(mixed $data, string $type, string $key): string {
-    $sz_fmt = static::PK_MAP[static::SZ_MAP[$type]];
-    $bin = pack($this->getPackType($type), $data);
-    return $type . $key . pack($sz_fmt, strlen($bin)) . $bin;
-  }
-
-  protected function unpackType(string $binary, string $type): mixed {
-    return unpack($this->getPackType($type), $binary)[1];
-  }
-
-  protected function getKeyType(string $key): string {
-    return ($this->config[$key] ?? BC_RAW);
-  }
-
-  protected function encodeKeyName(string $key): string {
-    return hex2bin(str_pad(dechex(($this->key_map[$key] ?? -1) + 1), 2, '0', STR_PAD_LEFT));
-  }
-
-  protected function decodeKeyName(string $bin): ?string {
-    return array_search(hexdec(bin2hex($bin)) - 1, $this->key_map);
-  }
-
-  protected function getDataType(mixed $data): string {
-    return match(true) {
-      is_int($data) && $data > 0 && $data < 255 => BC_UINT1,
-      is_int($data) && $data > -128 && $data < 127 => BC_INT1,
-      is_int($data) && $data > 0 && $data < 65535 => BC_UINT2,
-      is_int($data) && $data > -32767 && $data < 32768 => BC_INT2,
-      is_int($data) => BC_INT8,
-      is_float($data) => BC_DOUBLE,
-      is_bool($data) => BC_BOOL,
-      is_string($data) && is_numeric($data) && $data[0] !== '0' => BC_NUM,
-      is_string($data) => BC_STR,
-      is_array($data) && key($data) => BC_MAP,
-      is_array($data) => BC_LIST,
-      $data === null => BC_NULL,
-      default => BC_RAW,
+  protected function getDataFormat(mixed &$data): string {
+    $format = match(true) {
+      is_int($data) && $data > 0 && $data < 255 => 'C',
+      is_int($data) && $data > -128 && $data < 127 => 'c',
+      is_int($data) && $data > 0 && $data < 65535 => 'n',
+      is_int($data) && $data > -32768 && $data < 32767 => 's',
+      is_int($data) && $data > 0 && $data < 4294967295 => 'N',
+      is_int($data) && $data > -2147483648 && $data < 2147483647 => 'l',
+      is_int($data) && $data > 0 && $data < 4294967295 => 'N',
+      is_int($data) && $data > -9223372036854775808 && $data < 9223372036854775807 => 'q',
+      is_int($data) && $data > 0 && $data < 18446744073709551615 => 'J',
+      is_double($data) => 'E',
+      is_float($data) => 'G',
+      default => 'a',
     };
+
+    if ($format === 'a') {
+      switch (true) {
+        case is_string($data) && is_numeric($data) && $data[0] !== '0':
+          $val = gmp_strval(gmp_init($data, 10), 16);
+          if (strlen($val) % 2 !== 0) {
+            $val = '0' . $val;
+          }
+          $format = BC_NUM . 'a';
+          $data = hex2bin($val);
+          break;
+
+        case is_string($data) && trim($data, '0..9A..Fa..f') == '':
+          $format = 'H';
+          break;
+
+        case is_bool($data):
+          $format = BC_BOOL . 'C';
+          $data = $data ? 1 : 0;
+          break;
+
+        case is_null($data):
+          $format = BC_NULL . 'C';
+          $data = "\x01";
+          break;
+
+      }
+      $format .= strlen($data);
+    }
+
+    return $format;
   }
 
-  protected function getPackType(string $type): string {
-    return match($type) {
-      BC_HEX => 'H*',
-      BC_CHAR => 'c',
-      BC_UCHAR => 'C',
-      BC_INT1 => 'c',
-      BC_INT2 => 's',
-      BC_UINT1 => 'C',
-      BC_UINT2 => 'n',
-      BC_INT4 => 'l',
-      BC_UINT4 => 'N',
-      BC_INT8 => 'q',
-      BC_UINT8 => 'J',
-      BC_FLOAT => 'G',
-      BC_DOUBLE => 'E',
-      BC_LIST_UINT1 => 'C*',
-      BC_LIST_UINT2 => 'n*',
-      BC_LIST_UINT4 => 'N*',
-      BC_LIST_UINT8 => 'J*',
-      BC_LIST_INT1 => 'c*',
-      BC_LIST_INT2 => 's*',
-      BC_LIST_INT4 => 'l*',
-      BC_LIST_INT8 => 'q*',
-      default => 'a*',
-    };
-  }
+  // protected function getPackType(string $type): string {
+  //   return match($type) {
+  //     BC_HEX => 'H*',
+  //     BC_CHAR => 'c',
+  //     BC_UCHAR => 'C',
+  //     BC_INT4 => 'l',
+  //     BC_UINT4 => 'N',
+  //     BC_INT8 => 'q',
+  //     BC_UINT8 => 'J',
+  //     BC_FLOAT => 'G',
+  //     BC_DOUBLE => 'E',
+  //     BC_LIST_UINT1 => 'C*',
+  //     BC_LIST_UINT2 => 'n*',
+  //     BC_LIST_UINT4 => 'N*',
+  //     BC_LIST_UINT8 => 'J*',
+  //     BC_LIST_INT1 => 'c*',
+  //     BC_LIST_INT2 => 's*',
+  //     BC_LIST_INT4 => 'l*',
+  //     BC_LIST_INT8 => 'q*',
+  //     default => 'a*',
+  //   };
+  // }
 }
